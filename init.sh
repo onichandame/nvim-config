932f332c071fd6fdcd542b6e65b40e24c80f1a50
onichandame <zxinmyth@gmail.com>
2022-07-10 01:14:59 +0000

    chore: init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+yarn global add \
+	typescript \
+	graphql-language-service-cli \
+	prettier \
+	pyright \
+	typescript-language-server \
+	dockerfile-language-server-nodejs \
+	bash-language-server \
+	yaml-language-server \
+	vscode-langservers-extracted
