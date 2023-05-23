36d9fd5ebeb8ae7eef8f15767fe40eec20875f1b
onichandame <zxinmyth@gmail.com>
2023-05-23 11:09:23 +0800

    chore: fix

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -4,6 +4,6 @@ vim.o.cursorline=true --- enable current line highlight
 vim.o.cursorcolumn=true --- enable current column highlight
 vim.o.termguicolors=true --- required by status line and feline
 vim.o.hidden=true
-if(vim.fn.exists('+shellslash')) then
+if(vim.fn.exists('+shellslash')==1) then
 	vim.o.shellslash=true
 end
