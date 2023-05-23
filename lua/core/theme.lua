bc8c29a409d6f8b1aab6bd03c620649284404154
onichandame <zxinmyth@gmail.com>
2023-05-23 11:07:11 +0800

    chore: fix ssl in linux

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -4,4 +4,6 @@ vim.o.cursorline=true --- enable current line highlight
 vim.o.cursorcolumn=true --- enable current column highlight
 vim.o.termguicolors=true --- required by status line and feline
 vim.o.hidden=true
-vim.o.shellslash=true
+if(vim.fn.exists('+shellslash')) then
+	vim.o.shellslash=true
+end
