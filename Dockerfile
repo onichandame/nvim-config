74629dde792f36d34839b10d15665cdf600d6992
onichandame <zxinmyth@gmail.com>
2023-05-24 15:29:07 +0800

    chore: fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.18
 
 RUN apk update
-RUN apk add neovim nodejs ripgrep git gcc
+RUN apk add neovim neovim-doc nodejs ripgrep git gcc libstdc++-dev
 ADD . /root/.config/nvim 
 RUN nvim --headless "+Lazy! sync" +qa
--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -7,6 +7,9 @@ vim.o.hidden=true
 if(vim.fn.exists('+shellslash')==1) then
 	vim.o.shellslash=true
 end
+-- set shell for different OS
 if(vim.loop.os_uname().sysname=='Windows_NT')then
 	vim.o.shell='powershell'
+elseif (vim.fn.executable('ash')==1) then
+	vim.o.shell='ash'
 end
