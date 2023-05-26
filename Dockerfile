ddd6aa302e7c28976db170529a59769174b62258
onichandame <zxinmyth@gmail.com>
2023-05-26 05:05:20 +0000

    chore: add delete buffer mapping

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.18
 
 RUN apk update
-RUN apk add neovim neovim-doc nodejs npm ripgrep git gcc libstdc++-dev g++
+RUN apk add neovim neovim-doc nodejs npm ripgrep git gcc libstdc++-dev g++ fd
 ADD . /root/.config/nvim 
 RUN nvim --headless "+Lazy! sync" +qa
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,17 @@
 local builtin = require('telescope.builtin')
+local actions= require('telescope.actions')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+require('telescope').setup{
+	pickers={
+		buffers={
+			mappings={
+				n={
+					d=actions.delete_buffer
+				}
+			}
+		}
+	}
+}
