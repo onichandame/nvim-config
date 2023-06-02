56943ca0a64845f45bcce7f45091560aa7830de1
onichandame <zxinmyth@gmail.com>
2023-06-02 06:09:00 +0000

    fix: cmp Tab

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -80,7 +80,7 @@ cmp.setup({
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif vim.fn["vsnip#jumpable"](1) == 1 then
-				feedkey("<Plug>(vsnip-jump-next)", "")
+				vim.fn.feedkey("<Plug>(vsnip-jump-next)", "")
 			elseif has_words_before() then
 				cmp.complete()
 			else
