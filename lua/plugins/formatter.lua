29e308d82a6ca9cb55b4e05bcc0f6aa0966b8ee0
onichandame <zxinmyth@gmail.com>
2023-06-06 07:39:47 +0000

    fix: format

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,7 +1,2 @@
 vim.g.neoformat_try_node_exe =1
-vim.api.nvim_set_keymap('n','<leader>f',[[
-augroup fmt
-  autocmd!
-  autocmd BufWritePre * | Neoformat
-augroup END
-]],{})
+vim.api.nvim_set_keymap('n','<leader>f',':Neoformat<CR>',{})
