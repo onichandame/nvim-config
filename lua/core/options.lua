cf3363aa421e982a6b373207ecff6b99e507ea5b
onichandame <zxinmyth@gmail.com>
2023-06-09 08:56:37 +0000

    fix: et

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,7 @@
-vim.o.ic=true
-vim.o.tabstop=2
-vim.o.shiftwidth=0 -- when 0 defaults to tabstop
-vim.o.splitright=true
-vim.o.shell='bash'
-vim.o.foldlevelstart=99
+vim.o.ic = true
+vim.o.et = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 0 -- when 0 defaults to tabstop
+vim.o.splitright = true
+vim.o.shell = 'bash'
+vim.o.foldlevelstart = 99
