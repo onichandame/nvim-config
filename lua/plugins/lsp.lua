302b7e05c8755d1fe3c1e415616ab0bccb2a2e98
onichandame <zxinmyth@gmail.com>
2023-06-13 07:51:19 +0000

    fix: formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,26 +1,26 @@
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = {
-		'bash',
-		'tsx',
-		'yaml',
-		'rust',
-		'lua',
-		'typescript',
-		'javascript',
-		'vim',
-		'json5',
-		'html',
-		'prisma',
-		'graphql',
-		'go',
-		'dockerfile',
-		'css',
-		'markdown'
-	},
-	sync_install = true,
-	highlight = { enable = true },
-	indent = { enable = true },
-	autotag = { enable = true }
+  ensure_installed = {
+    'bash',
+    'tsx',
+    'yaml',
+    'rust',
+    'lua',
+    'typescript',
+    'javascript',
+    'vim',
+    'json5',
+    'html',
+    'prisma',
+    'graphql',
+    'go',
+    'dockerfile',
+    'css',
+    'markdown'
+  },
+  sync_install = true,
+  highlight = { enable = true },
+  indent = { enable = true },
+  autotag = { enable = true }
 }
 vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
@@ -28,13 +28,13 @@ vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 local npairs = require("nvim-autopairs")
 local Rule = require('nvim-autopairs.rule')
 npairs.setup({
-	check_ts = true,
-	ts_config = {
-		lua = { 'string', 'comment' }, -- it will not add a pair on that treesitter node
-		javascript = { 'template_string' },
-		typescript = {'string', 'template_string', 'source'},
-		java = false, -- don't check treesitter on java
-	}
+  check_ts = true,
+  ts_config = {
+    lua = { 'string', 'comment' }, -- it will not add a pair on that treesitter node
+    javascript = { 'template_string' },
+    typescript = { 'string', 'template_string', 'source' },
+    java = false, -- don't check treesitter on java
+  }
 })
 
 local ts_conds = require('nvim-autopairs.ts-conds')
@@ -43,88 +43,88 @@ require('mason').setup()
 
 -- press % => %% only while inside a comment or string
 npairs.add_rules({
-	Rule("%", "%", "lua")
-			:with_pair(ts_conds.is_ts_node({ 'string', 'comment' })),
-	Rule("$", "$", "lua")
-			:with_pair(ts_conds.is_not_ts_node({ 'function' })),
-	Rule("/**", "*/", { "javascript", "typescript", "typescriptreact", "javascriptreact" })
-			:with_pair(ts_conds.is_not_ts_node({ 'comment' })),
+  Rule("%", "%", "lua")
+      :with_pair(ts_conds.is_ts_node({ 'string', 'comment' })),
+  Rule("$", "$", "lua")
+      :with_pair(ts_conds.is_not_ts_node({ 'function' })),
+  Rule("/**", "*/", { "javascript", "typescript", "typescriptreact", "javascriptreact" })
+      :with_pair(ts_conds.is_not_ts_node({ 'comment' })),
 })
 
 local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local cmp = require 'cmp'
 cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-		end,
-	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif vim.fn["vsnip#jumpable"](1) == 1 then
-				vim.fn.feedkey("<Plug>(vsnip-jump-next)", "")
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-			end
-		end, { "i", "s" }),
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' }, -- For vsnip users.
-		-- { name = 'luasnip' }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-	}, {
-		{ name = 'buffer' },
-	})
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#jumpable"](1) == 1 then
+        vim.fn.feedkey("<Plug>(vsnip-jump-next)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-	}, {
-		{ name = 'buffer' },
-	})
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = 'buffer' }
-	}
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{ name = 'cmdline' }
-	})
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
 })
 
 -- Setup lspconfig.
@@ -137,104 +137,107 @@ vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<C
 vim.api.nvim_set_keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>w', '<cmd>lua vim.lsp.buf.format()<CR><cmd>:w<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>w',
+  '<cmd>lua vim.lsp.buf.format{timeout_ms=10000, filter = function(client) return client.name ~= "tsserver" end }<CR><cmd>:w<CR>',
+  opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl',
+    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 end
 
 local handlers = {
-	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' }),
+  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' }),
 }
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'marksman', 'tsserver', 'dockerls', 'lua_ls', 'prismals', 'bashls', 'html', 'pyright','graphql' }
+local servers = { 'marksman', 'tsserver', 'dockerls', 'lua_ls', 'prismals', 'bashls', 'html', 'pyright', 'graphql' }
 for _, lsp in pairs(servers) do
-	require('lspconfig')[lsp].setup {
-		on_attach = on_attach,
-		handlers = handlers,
-		capabilities = capabilities,
-	}
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    handlers = handlers,
+    capabilities = capabilities,
+  }
 end
 
 require('lspconfig').jsonls.setup {
-	on_attach = on_attach,
-	handlers = handlers,
-	capabilities = capabilities,
-	settings = {
-		json = {
-			schemas = require('schemastore').json.schemas(),
-			validate = { enable = true },
-		},
-	},
+  on_attach = on_attach,
+  handlers = handlers,
+  capabilities = capabilities,
+  settings = {
+    json = {
+      schemas = require('schemastore').json.schemas(),
+      validate = { enable = true },
+    },
+  },
 }
 
 local yaml_schema = require('schemastore').yaml.schemas()
-yaml_schema['kubernetes']={
-	"/*secret.yaml",
-	"/*secret.yml",
-	"/*sts.yaml",
-	"/*sts.yml",
-	"/*svc.yaml",
-	"/*svc.yml",
-	"/*deploy.yaml",
-	"/*deploy.yml",
-	"/*cm.yaml",
-	"/*cm.yml",
-	"/*ns.yaml",
-	"/*ns.yml",
-	"/*role.yaml",
-	"/*role.yml",
-	"/*sa.yaml",
-	"/*sa.yml",
-	"/*sc.yaml",
-	"/*sc.yml",
-	"/*pv.yaml",
-	"/*pv.yml",
-	"/*pvc.yaml",
-	"/*pvc.yml",
+yaml_schema['kubernetes'] = {
+  "/*secret.yaml",
+  "/*secret.yml",
+  "/*sts.yaml",
+  "/*sts.yml",
+  "/*svc.yaml",
+  "/*svc.yml",
+  "/*deploy.yaml",
+  "/*deploy.yml",
+  "/*cm.yaml",
+  "/*cm.yml",
+  "/*ns.yaml",
+  "/*ns.yml",
+  "/*role.yaml",
+  "/*role.yml",
+  "/*sa.yaml",
+  "/*sa.yml",
+  "/*sc.yaml",
+  "/*sc.yml",
+  "/*pv.yaml",
+  "/*pv.yml",
+  "/*pvc.yaml",
+  "/*pvc.yml",
 }
 
 require('lspconfig').yamlls.setup {
-	on_attach = on_attach,
-	handlers = handlers,
-	capabilities = capabilities,
-	settings = {
-		yaml = {
-			schemas = yaml_schema
-		},
-		validate = { enable = true }
-	}
+  on_attach = on_attach,
+  handlers = handlers,
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      schemas = yaml_schema
+    },
+    validate = { enable = true }
+  }
 }
 
 require 'rust-tools'.setup({
-	server = {
-		on_attach = on_attach,
-		handlers = handlers,
-		capabilities = capabilities,
-	}
+  server = {
+    on_attach = on_attach,
+    handlers = handlers,
+    capabilities = capabilities,
+  }
 })
 
-require('mason-lspconfig').setup{
-	automatic_installation=true
+require('mason-lspconfig').setup {
+  automatic_installation = true
 }
