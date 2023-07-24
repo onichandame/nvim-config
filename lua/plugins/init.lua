5f0f9247533f3934a935180d23c28c248a5ff2e8
onichandame <zxinmyth@gmail.com>
2023-07-24 06:56:29 +0000

    Move lua_ls to container

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- lsp
   'neovim/nvim-lspconfig',
+  'lspcontainers/lspcontainers.nvim',
   'tamago324/nlsp-settings.nvim',
   {
     "williamboman/mason.nvim",
