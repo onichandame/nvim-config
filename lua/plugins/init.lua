1873a2ee619ec99bb9219b99da0f8ce06d0e01ad
onichandame <zxinmyth@gmail.com>
2023-07-25 16:08:17 +0000

    Add zf sorter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -66,10 +66,15 @@ require('lazy').setup({
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  },{
+  },
+  {
+    "natecraddock/telescope-zf-native.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", }
+  },
+  {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-},
+  },
   -- theme
   'lifepillar/vim-solarized8',
   -- misc
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,28 @@
 local builtin = require('telescope.builtin')
-local actions= require('telescope.actions')
+local actions = require('telescope.actions')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fe', ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
-require('telescope').setup{
-	pickers={
-		buffers={
-			mappings={
-				n={
-					d=actions.delete_buffer
-				}
-			}
-		}
-	}
+require('telescope').setup {
+  pickers = {
+    buffers = {
+      mappings = {
+        n = {
+          d = actions.delete_buffer
+        }
+      }
+    }
+  },
+  extensions = {
+    file_browser = {
+      hidden = { file_browser = true, folder_browser = true },
+      auto_depth = true,
+      hide_parent_dir = true,
+      grouped = true,
+    }
+  }
 }
+require 'telescope'.load_extension 'zf-native'
 require 'telescope'.load_extension 'file_browser'
