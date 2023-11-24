350bde4a6d3adc64cb56fc42fb5e9cd1bd996e5a
xiao <zxinmyth@gmail.com>
2023-11-24 17:05:24 +0800

    rearrange statusbar sections

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -11,12 +11,12 @@ require('lualine').setup({
   options = { theme = require('lualine.themes.solarized_dark') },
   sections = {
     lualine_c = { 'lsp_progress' },
-    lualine_x = { 'filename', 'encoding', 'filetype' },
-    lualine_y = { function()
+    lualine_x = { function()
       return 'cpu: ' .. cpu_usage() .. '%%'
     end, function()
       return 'mem: ' .. mem_usage() .. '%%'
     end },
+    lualine_y = { 'filename', 'encoding', 'filetype' },
     lualine_z = { 'progress', 'location', },
   }
 }) --- status line
