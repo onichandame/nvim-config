532e2f49f6272893ee117cd79cd88a537fdf9ad2
xiao <zxinmyth@gmail.com>
2023-11-24 16:52:59 +0800

    add cpu/mem usage

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -1,7 +1,22 @@
+local function cpu_usage()
+  return string.gsub(vim.fn.system("top -bn1 | grep 'Cpu(s)' | awk '{print $2}'"), '\n', '')
+end
+
+local function mem_usage()
+  local raw = string.gsub(vim.fn.system("free | grep Mem | awk '{print $3/$2 * 100.0}'"), '\n', '')
+  return string.format("%.2f", tonumber(raw))
+end
+
 require('lualine').setup({
-	options = { theme = require('lualine.themes.solarized_dark') },
-	sections = {
-		lualine_c = { 'lsp_progress' },
-		lualine_x = { 'filename', 'encoding','filetype' },
-	}
+  options = { theme = require('lualine.themes.solarized_dark') },
+  sections = {
+    lualine_c = { 'lsp_progress' },
+    lualine_x = { 'filename', 'encoding', 'filetype' },
+    lualine_y = { function()
+      return 'cpu: ' .. cpu_usage() .. '%%'
+    end, function()
+      return 'mem: ' .. mem_usage() .. '%%'
+    end },
+    lualine_z = { 'progress', 'location', },
+  }
 }) --- status line
