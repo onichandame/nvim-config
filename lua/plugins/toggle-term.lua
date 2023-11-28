f0c9e21dfa7e01e7ebe992b3672b0ba5bb05f2bd
xiao <zxinmyth@gmail.com>
2023-11-28 16:39:12 +0800

    toggleterm in insert mode

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,4 +1,2 @@
-require("toggleterm").setup({
-	start_in_insert=false,
-})
-vim.api.nvim_set_keymap('n','<leader>t',':ToggleTerm<CR>',{silent=false})
+require("toggleterm").setup({})
+vim.api.nvim_set_keymap('n', '<leader>t', ':ToggleTerm<CR>', { silent = false })
