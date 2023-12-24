6593095f071849a4d11693b3450ac05dfc8941c0
xiao <zxinmyth@gmail.com>
2023-12-24 15:19:46 +0800

    feat: add textcase

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -104,6 +104,20 @@ require('lazy').setup({
   'mzlogin/vim-markdown-toc',
   'ekalinin/dockerfile.vim',
   'ojroques/nvim-osc52',
+  {
+    "johmsalas/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    -- Author's Note: If default keymappings fail to register (possible config issue in my local setup),
+    -- verify lazy loading functionality. On failure, disable lazy load and report issue
+    -- lazy = false,
+    config = function()
+      require("textcase").setup({})
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "v" }, desc = "Telescope" },
+    },
+  }
 })
 
 -- Plugin configs
