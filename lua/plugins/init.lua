f94d365125d3a37286318bedf99cfa50ecb320b0
xiao <zxinmyth@gmail.com>
2024-02-17 15:58:45 +0800

    add args to live grep finder

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -87,7 +87,16 @@ require('lazy').setup({
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    }
+  },
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    -- This will not install any breaking changes.
+    -- For major updates, this must be adjusted manually.
+    version = "^1.0.0",
+    dependencies = { "nvim-telescope/telescope.nvim", },
   },
   {
     "natecraddock/telescope-zf-native.nvim",
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
+local telescope = require('telescope')
+local lga_actions = require("telescope-live-grep-args.actions")
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', telescope.extensions.live_grep_args.live_grep_args, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fe', ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
@@ -16,11 +19,29 @@ require('telescope').setup {
       mappings = {
         n = {
           d = actions.delete_buffer
+        },
+        i = {
+          ["<C-k>"] = lga_actions.quote_prompt(),
+          ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
         }
       }
     },
   },
   extensions = {
+    live_grep_args = {
+      auto_quoting = true, -- enable/disable auto-quoting
+      -- define mappings, e.g.
+      mappings = {         -- extend mappings
+        i = {
+          ["<C-k>"] = lga_actions.quote_prompt(),
+          ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+        },
+      },
+      -- ... also accepts theme settings, for example:
+      -- theme = "dropdown", -- use dropdown theme
+      -- theme = { }, -- use own theme spec
+      -- layout_config = { mirror=true }, -- mirror preview pane
+    },
     file_browser = {
       hidden = { file_browser = true, folder_browser = true },
       auto_depth = true,
@@ -39,3 +60,4 @@ autocmd User TelescopePreviewerLoaded setlocal number
 require 'telescope'.load_extension 'zf-native'
 require 'telescope'.load_extension 'file_browser'
 require 'telescope'.load_extension 'quicknote'
+require 'telescope'.load_extension 'live_grep_args'
